c522811ba969e778e12161d34751a25c51967d14
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-04 22:22:43 +0900

    adding signin pages style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
-import Header from './components/header/header.component';
 import HomePage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-sign-up.component';
+import Header from './components/header/header.component';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
