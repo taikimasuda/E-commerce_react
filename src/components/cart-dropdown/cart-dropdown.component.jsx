cc0a62ec29b07fa478d84a7dde92c05ed3ce7972
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-10 13:09:03 +0900

    updating our application with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.style.scss';
-import FormInput from '../form-input/form-input.component';
 
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
@@ -16,8 +16,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
-})
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
+});
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
-import { addItem } from '../../redux/cart/cart.action';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.style.scss';
 
 const CollectionItem = ({ item, addItem }) => {
