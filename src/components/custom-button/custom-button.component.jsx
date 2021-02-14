8f8efa63769a980148b88e82e905ef12b1b12dd0
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-14 20:04:27 +0900

    few changesfor styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import './custom-button.style.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${inverted ? 'inverted' : ''}, ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+import './homepage.style.scss';
 
 const HomePage = () => (
   <div className='homepage'>
