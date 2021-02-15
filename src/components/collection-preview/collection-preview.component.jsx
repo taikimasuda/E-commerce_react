e330916de02b009ad1fdc100972d17869c3d3c2a
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-15 11:09:37 +0900

    added loading files that appear when fetching data from firebase

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.style.scss';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {
-        items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))
-      }
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
 
-export default CollectionPreview;
-
+export default withRouter(CollectionPreview);
